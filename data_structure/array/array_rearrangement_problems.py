779df56493924408132d773de30c3346b599e5db
bitansaha <bitansaha.1@gmail.com>
2019-05-22 00:24:25 -0500

    adding array arrangement problems 7, 8, 9

--- data_structure/array/array_rearrangement_problems.py
+++ data_structure/array/array_rearrangement_problems.py
@@ -343,6 +343,9 @@ Source: https://www.geeksforgeeks.org/move-zeroes-end-array-set-2-using-single-t
 '''
 
 
+# Solution 1:
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def problem5_solution1(int_array):
     zero_count = 0
     for index in range(len(int_array)):
@@ -371,6 +374,9 @@ Source: https://www.geeksforgeeks.org/minimum-swaps-required-bring-elements-less
 '''
 
 
+# Solution 1:
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def problem6_solution1(arr, k):
     count = 0
 
@@ -394,8 +400,220 @@ def problem6_solution1(arr, k):
     return count - max_count
 
 
+# if __name__ == '__main__':
+#     original_arr = [2, 1, 5, 6, 3]
+#     arr = original_arr.copy()
+#     result = problem6_solution1(arr, 3)
+#     print(result)
+
+''' -------------------------------------------------------------------------------------------------------------- '''
+
+'''
+Problem 7: Rearrange an array in order – smallest, largest, 2nd smallest, 2nd largest
+Source: https://www.geeksforgeeks.org/rearrange-array-order-smallest-largest-2nd-smallest-2nd-largest/
+'''
+
+
+# Solution 1:
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def problem7_solution1(arr):
+    arr_copy = arr.copy()
+    arr_copy.sort()
+    right_index = len(arr) - 1
+    left_index = 0
+
+    for index in range(len(arr)):
+        if index % 2 == 0:
+            arr[index] = arr_copy[left_index]
+            left_index += 1
+        else:
+            arr[index] = arr_copy[right_index]
+            right_index -= 1
+
+
+# Solution 2:
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def problem7_solution2(arr):
+    arr.sort()
+    max_value = arr[len(arr) - 1] + 1
+    left_index = 0
+    right_index = len(arr) - 1
+
+    for index in range(len(arr)):
+        if index % 2 == 0:
+            arr[index] += (arr[left_index] % max_value) * max_value
+            left_index += 1
+        else:
+            arr[index] += (arr[right_index] % max_value) * max_value
+            right_index -= 1
+
+    for index in range(len(arr)):
+        arr[index] = arr[index] // max_value
+
+
+# if __name__ == '__main__':
+#     original_arr = [5, 8, 1, 4, 2, 9, 3, 7, 6]
+#     arr = original_arr.copy()
+#     problem7_solution1(arr)
+#     print(*arr)
+#     arr = original_arr.copy()
+#     problem7_solution2(arr)
+#     print(*arr)
+
+''' -------------------------------------------------------------------------------------------------------------- '''
+
+'''
+Problem 8: Reorder an array according to given indexes
+    Given two integer arrays of same size, “arr[]” and “index[]”, reorder elements in “arr[]” according to given index 
+    array. It is not allowed to given array arr’s length.
+Source: https://www.geeksforgeeks.org/reorder-a-array-according-to-given-indexes/
+'''
+
+
+# Solution 1:
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def problem8_solution1(arr, index):
+    arr_copy = [None] * len(arr)
+    for index_ptr in range(len(index)):
+        arr_copy[index[index_ptr]] = arr[index_ptr]
+
+    for index_ptr in range(len(index)):
+        arr[index_ptr] = arr_copy[index_ptr]
+        index[index_ptr] = index_ptr
+
+
+# Solution 2:
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def problem8_solution2(arr, index):
+    for index_ptr in range(len(index)):
+        if index[index_ptr] != index_ptr:
+            ptr = index_ptr
+            shift_ptr = index[ptr]
+            value = arr[ptr]
+            while True:
+                temp = arr[shift_ptr]
+                arr[shift_ptr] = value
+                value = temp
+
+                temp = index[shift_ptr]
+                index[shift_ptr] = shift_ptr
+
+                if shift_ptr == ptr:
+                    break
+                shift_ptr = temp
+
+
+# if __name__ == '__main__':
+#     original_arr = [50, 40, 70, 60, 90]
+#     original_index = [3,  0,  4,  1,  2]
+#     arr = original_arr.copy()
+#     index = original_index.copy()
+#     problem8_solution1(arr, index)
+#     print(*arr)
+#     print(*index)
+#     arr = original_arr.copy()
+#     index = original_index.copy()
+#     problem8_solution2(arr, index)
+#     print(*arr)
+#     print(*index)
+
+''' -------------------------------------------------------------------------------------------------------------- '''
+
+'''
+Problem 9: Rearrange positive and negative numbers with constant extra space
+    Given an array of positive and negative numbers, arrange them such that all negative integers appear before all the 
+    positive integers in the array. The order of appearance should be maintained
+Source: https://www.geeksforgeeks.org/rearrange-positive-and-negative-numbers/
+'''
+
+
+# Solution 1: Right Rotate
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+def problem9_solution1(arr):
+    last_negative_index = -1
+    for index in range(len(arr)):
+        if arr[index] < 0:
+            right_rotate(arr, last_negative_index + 1, index)
+            last_negative_index += 1
+
+
+# Solution 2: Extra Memory
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def problem9_solution2(arr):
+    arr_copy = [None] * len(arr)
+    negative_index = -1
+    positive_index = len(arr)
+
+    for index in range(len(arr)):
+        if arr[index] < 0:
+            negative_index += 1
+            arr_copy[negative_index] = arr[index]
+        else:
+            positive_index -= 1
+            arr_copy[positive_index] = arr[index]
+
+    index = -1
+    for value in arr_copy[:negative_index + 1]:
+        index += 1
+        arr[index] = value
+
+    for value in reversed(arr_copy[positive_index:]):
+        index += 1
+        arr[index] = value
+
+
+# Solution 3: Modular Arithmetic
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def problem9_solution3(arr):
+    min_value = sys.maxsize
+    max_value = -sys.maxsize - 1
+    negative_count = -1
+
+    # retrieving the min, max values of the array
+    for value in arr:
+        if value < 0:
+            negative_count += 1
+        if value < min_value:
+            min_value = value
+        if value > max_value:
+            max_value = value
+
+    # adding the minimum value to all indexes to make it an array of positive integers
+    for index in range(len(arr)):
+        arr[index] -= min_value
+
+    max_value = (max_value - min_value) + 1
+
+    negative_index = -1
+    positive_index = negative_count
+
+    for index in range(len(arr)):
+        if ((arr[index] % max_value) + min_value) < 0:
+            negative_index += 1
+            arr[negative_index] += (arr[index] % max_value) * max_value
+        else:
+            positive_index += 1
+            arr[positive_index] += (arr[index] % max_value) * max_value
+
+    for index in range(len(arr)):
+        arr[index] = (arr[index] // max_value) + min_value
+
+
 if __name__ == '__main__':
-    original_arr = [2, 1, 5, 6, 3]
+    original_arr = [12, 11, -13, -5, 6, -7, 5, -3, -6]
     arr = original_arr.copy()
-    result = problem6_solution1(arr, 3)
-    print(result)
+    problem9_solution1(arr)
+    print(*arr)
+    arr = original_arr.copy()
+    problem9_solution2(arr)
+    print(*arr)
+    arr = original_arr.copy()
+    problem9_solution3(arr)
+    print(*arr)
--- data_structure/test.py
+++ data_structure/test.py
@@ -2,5 +2,8 @@
 print('HELLO')
 arr = [1, 2, 3, 4]
 
-for index in range(2, len(arr), 1):
-    print(index)
+# for index in range(2, len(arr), 1):
+#     print(index)
+# arr[2:].reverse()
+arr[:].reverse()
+print(arr)
